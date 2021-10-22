53351c0e4fa574ecffc965ce47d287518ed5f5c2
salsanadhifaa <91627039+salsanadhifaa@users.noreply.github.com>
2021-10-22 12:59:57 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,46 @@
+class Main {
+    public static void main(String[] args) {
+      Heroes hero = new Heroes();
+  
+      hero.setProfile("Salsa", 21, 159);
+      hero.profile();
+      
+    
+      hero.jalan();
+
+      hero.lari();
+      hero.lari();
+
+      hero.makan();
+
+      hero.minum();
+      hero.minum();
+
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      
+      hero.duduk();
+      hero.duduk();
+      hero.duduk();
+      hero.duduk();
+        
+      
+      hero.profile();
+   
+      // challenge:
+      /*
+     
+      Group 9: jalan 1x, lari 2x, makan 1x, minum 2x, lompat 8x, duduk 4x
+      */
+        
+      // tampilkan status terkini...
+      hero.profile();
+      
+    }
+  }
